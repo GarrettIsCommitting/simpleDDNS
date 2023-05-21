e6fe2bbba16717df066d8a8ab8d0464e2255f47d
Garrett Allen <g@garrettis.online>
2023-05-21 10:43:47 -0700

    README first draft Setup config access

---
+++ app/config.py
@@ -0,0 +1,23 @@
+# app/config.py
+
+import os
+
+from pydantic import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    """ Provide access to needed env parameters
+        :param str domain: The root domain used for DDNS
+        :param str record: The A record used for DDNS
+        :param str dns_provider: The URL for the DNS provider's API
+        :param str api_key: The api key for the DNS provider
+        :param str zone_id: The zone id (if needed) for the DNS zone to manage
+    """
+    domain: str = Field('example.com', env='DYN_DOMAIN')
+    record: str = Field('ddns', env='DYN_RECORD')
+    dns_provider: str = Field('https://api.cloudflare.com', env="DNS_PROVIDER")
+    api_key: str = Field('fake_key', env='API_KEY')
+    zone_id: str = Field('fake_zone_id', env="ZONE_ID")
+
+
+settings = Settings()
